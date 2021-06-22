088acd1ebc4ee8a5a8cf7fece4b3587d9acc66f8
pratiksharm <sharma.pratik2016@gmail.com>
2021-06-23 00:13:34 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 'use strict';
 
 const Express = require('express');
+const cors = require('cors');
+
 const router = require('./lib');
 
 const app = Express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
+app.use(cors());
 app.use('/api/', router);
 
 app.listen(port, function () {
